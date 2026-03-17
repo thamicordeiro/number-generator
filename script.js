cf34b050bdbfba304699f39ff77b8257fa878a4e
Thamires Cordeiro <thamirescordeiro1411@outlook.com>
2026-03-17 11:30:19 -0300

    iniciando JS

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const numero = document.getElementById("numero")
 const minimo = document.getElementById("minimo")
 const maximo = document.getElementById("maximo")
 const message = document.getElementById("mensagem")
+
+const form = document.querySelector("form")
 const button = document.querySelector(".btn-primary")
+
+form.addEventListener ("submit", function (event)  {
+  event.preventDefault() 
+
 
 if (!numero.value || !minimo.value || !maximo.value) {
   
   message.textContent = "Preencha todos os campos"
-
-  message.classList.remove("success")
-  message.classList.add("error")
-
+  return
 }
+ 
+if (Number(maximo.value) <= Number(minimo.value)) {
+  message.textContent = "Digite um valor maior que o mínimo"
+  return
+}
+
+})
